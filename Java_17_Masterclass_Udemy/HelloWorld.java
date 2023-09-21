5e82525e69497125f99cb7f99a40b27f8266d48d
rooftopcaat <heedong9849@gmail.com>
2023-09-21 16:19:28 +0900

    Hello World!

--- Java_17_Masterclass_Udemy/HelloWorld.java
+++ Java_17_Masterclass_Udemy/HelloWorld.java
@@ -1,6 +1,20 @@
 
 public class Java_17_Masterclass_Udemy {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello W");
     }
 }
+
+// public = access modifier
+// 코드의 다른 부분이나 다른 사람의 코드가 요소에 엑세스하는
+// 범위를 정의한다
+// public은 '완전 공개'로 누구나 사용 가능함
+// private는 클래스 외부에선 아무나 접근할 수 없음. 그 집의 식구만
+// 사용 가능
+
+// class = 클래스를 정의하는 데 사용
+
+// method = 메서드는 연산을 수행하는 하나 이상의 구문 집합
+// 자바 코드 진입점으로 프로그램을 실행할 메인 메서드를 찾음
+
+// void = 메서드가 어떤 정보도 반환하지 않음
