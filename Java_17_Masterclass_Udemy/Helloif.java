058095bb079acff01d996012661ab06b5ce68b50
rooftopcaat <heedong9849@gmail.com>
2023-09-21 16:29:20 +0900

    my name is rooftopcaat

---
+++ Java_17_Masterclass_Udemy/Helloif.java
@@ -0,0 +1,13 @@
+public class Java_17_Masterclass_Udemy {
+    public static void main(String[] args) {
+        System.out.println("Hello, rooftopcaat");
+
+        boolean isDog = false;
+        if (isDog == false) {
+            System.out.println("I'm not a dog!");
+        }
+        else
+            System.out.println("Yes! my name is rooftopcaat.");
+
+    }
+}
