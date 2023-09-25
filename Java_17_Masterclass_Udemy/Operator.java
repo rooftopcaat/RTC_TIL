29193c6b9737b6d5a6d8836bd255ab5d07bcd345
rooftopcaat <heedong9849@gmail.com>
2023-09-25 13:49:55 +0900

    remain

---
+++ Java_17_Masterclass_Udemy/Operator.java
@@ -0,0 +1,17 @@
+public class Operator {
+    public static void main(String[] args) {
+        double a = 20.00d;
+        double b = 80.00d;
+        double c = (a + b) * 100.00d;
+        System.out.println("Total = " + c);
+        double d = c % 40.00d;
+        System.out.println("remainder =" + d);
+
+        boolean noRemainder = (d == 0) ? true : false;
+        System.out.println("d = " + noRemainder);
+
+        if (!noRemainder) {
+            System.out.println("Got some remainder");
+        }
+    }
+}
