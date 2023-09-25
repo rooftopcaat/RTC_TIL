ec341991225a729cb1641f4618db0b2361756ce4
rooftopcaat <heedong9849@gmail.com>
2023-09-25 16:34:46 +0900

    Express_Statement2

---
+++ Java_17_Masterclass_Udemy/Ex2_Express_State.java
@@ -0,0 +1,22 @@
+public class Ex2_Express_State {
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 10000;
+        int levelCompleted = 8;
+        int bonus = 200;
+
+//        int finalScore = score;
+//
+//        if (gameOver == true) {
+//            finalScore += (levelCompleted * bonus);
+//            System.out.println("Your final score was " + finalScore);
+//        }
+        int lastScore = score;
+
+        if (gameOver == true) {
+            lastScore += (levelCompleted * bonus);
+            System.out.println("Your lastScore was " + lastScore);
+        }
+    }
+}
